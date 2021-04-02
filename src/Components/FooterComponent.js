4b1bcb768787ef32574bcde97ec97f0d429fb1ce
nidhipanwar13 <pnwrnidhi@gmail.com>
2021-04-02 16:58:12 +0530

    correction of links in footer

--- src/Components/FooterComponent.js
+++ src/Components/FooterComponent.js
@@ -19,17 +19,17 @@ class Footer extends Component {
                         </div>
                         <div className="col-sm col-md-4"><br />
                             <h5><b>Connect with us</b></h5><br />
-                                <SocialIcon url="https://www.linkedin.com/" style={{ height: 35, width: 35 }} bgColor="#0077B5" fgColor="#FFFFFF" />&emsp;
+                            <SocialIcon url="https://www.linkedin.com/" style={{ height: 35, width: 35 }} bgColor="#0077B5" fgColor="#FFFFFF" />&emsp;
                                 <SocialIcon url="https://www.youtube.com/" style={{ height: 35, width: 35 }} bgColor="#BB001B" fgColor="#FFFFFF" />&emsp;
                                 <SocialIcon url="https://twitter.com/" style={{ height: 35, width: 35 }} bgColor="#0077B5" fgColor="#FFFFFF" />
                         </div>
-                        
+
                         <div className="col-sm col-md-4"><br />
                             <h5><b>Write us on</b></h5><br />
                             <SocialIcon url="mailto:info@Miluzstudio.com" style={{ height: 35, width: 35 }} bgColor="#BB001B" fgColor="#FFFFFF" />
                         </div>
                     </div>
-                    <p className="copyright justify-content-md-left"> <b>© Copyright 2021 Miluz_studio</b></p><br/>
+                    <p className="copyright justify-content-md-left"> <b>© Copyright 2021 Miluz_studio</b></p><br />
                 </div>
             </div>
         );
